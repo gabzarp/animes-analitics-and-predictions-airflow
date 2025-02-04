324119fa4e4a17967c202b2e9021fe24b1e2ba05
Gabriel Zarpelon <gabriel.zarpelon@mimo.com.br>
2025-02-04 01:34:34 -0300

    api dags

--- dag_my_anime_list_api.py
+++ dag_my_anime_list_api.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from airflow.models.dag import DAG
 import pendulum
 
@@ -6,14 +10,14 @@ from airflow.operators.bash import (
 )
 
 with DAG(
-    dag_id="dag_test",
+    dag_id="dag_my_anime_list_api",
     schedule=None,
     start_date=pendulum.datetime(2024, 9, 16, tz="UTC"),
     catchup=False
 ):
     get_data_from_api = BashOperator(
         task_id = 'get_data_from_api',
-        bash_command = "python3 ~/airflow/python/get_data_from_api.py"
+        bash_command = f"python3 {os.getenv('PYTHON_DIRECTORY')}/get_data_from_api.py"
     )
     
     
